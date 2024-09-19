7759c4938cb5533adac0cd811227121b904a8cf8
hangilzzang <gkrwhddlwjqrl@gmail.com>
2024-09-19 19:11:26 +0900

    20240919

--- Assets/QuarterView.cs
+++ Assets/QuarterView.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class QuarterView : MonoBehaviour
 {
-    float offset = 10;
+    public float offset = 10;
+    public Vector3 target;
 
     void LateUpdate()
     {
         // 카메라의 위치를 Vector3.zero에서 offset만큼 떨어진 위치로 설정
-        transform.position = new Vector3(offset, offset, offset);
+        transform.position = new Vector3(target.x + offset, target.y + offset, target.z + offset);
         // 카메라가 항상 (0, 0, 0)을 바라보도록 설정
-        transform.LookAt(Vector3.zero);
+        transform.LookAt(target);
     }
 
 }
